2fa4678c627115a975cad2556cf147f07469e9a4
muditsingal <muditsingal@gmail.com>
2023-06-13 14:18:07 +0530

    Improved problem 5 efficiency

--- eq_row_col_pairs/pairs_numpy.py
+++ eq_row_col_pairs/pairs_numpy.py
@@ -3,11 +3,8 @@ import numpy as np
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        if n == 1:
-            return 1
-
         grid_arr = np.array(grid)
-        grid_T = grid_arr.copy().T
+        grid_T = grid_arr.T
         n_pairs = 0
         for i in range(n):
             n_pairs += (n - np.count_nonzero(np.sum(np.abs(grid_T - np.roll(grid_arr, shift=i, axis=1)), axis=0)))
