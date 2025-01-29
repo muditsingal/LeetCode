41f0ce6eeceb2afa911fad9de060825f235d92d3
muditsingal <muditsingal@gmail.com>
2025-01-29 00:10:03 -0500

    Added shortest path using bfs code

--- graph/adjacency_list.cpp
+++ graph/adjacency_list.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <unordered_set>
 #include <unordered_map>
+#include <queue>
+#include <stack>
+#include <utility>
 
 using namespace std;
 
@@ -33,6 +37,56 @@ public:
             cout << "\n";
         }
     }
+
+    vector<int> shortest_path_bfs(int start, int goal)
+    {
+        vector<int> res;
+        unordered_set<int> visited;
+        unordered_map<int, int> parent;
+        queue<int> node_q;
+        int curr;
+        if(adj_list[start].size() == 0)
+        {
+            cout << "Invalid start node" << endl;
+            return res;
+        }
+        else if(adj_list[goal].size() == 0)
+        {
+            cout << "Invalid goal node" << endl;
+            return res;
+        }
+        visited.insert(start);
+        node_q.push(start);
+        parent[start] = -1;
+        while(!node_q.empty())
+        {
+            curr = node_q.front();
+            node_q.pop();
+            if(curr == goal)
+                break;
+            for(auto &nbr : adj_list[curr])
+            {
+                if(visited.count(nbr) == 0)
+                {
+                    parent[nbr] = curr;
+                    visited.insert(nbr);
+                    node_q.push(nbr);
+                }
+            }
+        }
+
+        while(curr != -1)
+        {
+            res.push_back(curr);
+            curr = parent[curr];
+        }
+
+        for(int i = 0; i < res.size()/2; i++)
+            swap(res[i], res[res.size()-i-1]);
+
+        return res;
+    }
+
     void print_adj_mtx()
     {
         for(int i = 0; i < adj_mtx.size(); i++)
@@ -57,21 +111,30 @@ int main()
     graph g1(n);
 
     edges.push_back(make_pair(0, 1));
-    edges.push_back(make_pair(0, 4));
-    edges.push_back(make_pair(1, 2));
-    edges.push_back(make_pair(1, 3));
-    edges.push_back(make_pair(2, 3));
-    edges.push_back(make_pair(3, 4));
+    edges.push_back(make_pair(0, 2));
+    edges.push_back(make_pair(0, 3));
 
-    for(int i = 0; i < m; i++)
-    {
-        // add edge to the graph
+    edges.push_back(make_pair(1, 4));
+    edges.push_back(make_pair(2, 5));
+    edges.push_back(make_pair(3, 10));
+
+    edges.push_back(make_pair(4, 7));
+    edges.push_back(make_pair(5, 8));
+    edges.push_back(make_pair(6, 7));
+    edges.push_back(make_pair(7, 10));
+    edges.push_back(make_pair(8, 9));
+    edges.push_back(make_pair(9, 10));
+
+
+    for(int i = 0; i < edges.size(); i++)
         g1.add_edge(edges[i].first, edges[i].second);
-        g1.add_adj_mtx(edges[i].first, edges[i].second);
-    }
 
-    g1.print_adj_list();
-    g1.print_adj_mtx();
+    // g1.print_adj_list();
+    vector<int> path = g1.shortest_path_bfs(0, 10);
+    for(auto &node : path)
+        cout << node << " ";
+
+    cout << endl;
 
     return 0;
 }
--- graph/directed_graph.cpp
+++ graph/directed_graph.cpp
@@ -5,6 +5,7 @@
 #include <map>
 #include <unordered_map>
 #include <stack>
+#include <queue>
 
 using namespace std;
 
@@ -39,6 +40,86 @@ public:
         }
         topo_stk.push(node);
     }
+    vector<int> kahn_topo_sort(int v, int e)
+    {
+        queue<int> topo_q;
+        vector<int> indegree(v);
+        vector<int> res;
+        int curr;
+        // Find initial indegree vector
+        for(auto &node : adj_list)
+            for(auto &nbr : node.second)
+                indegree[nbr] += 1;
+
+        // Push node(s) with 0 indegree into queue
+        for(int i = 0; i < indegree.size(); i++)
+        {
+            if(indegree[i] == 0)
+                topo_q.push(i);
+        }
+
+        while(!topo_q.empty())
+        {
+            // Pop the front of queue
+            curr = topo_q.front();
+            topo_q.pop();
+
+            // Update answer with popped element of queue
+            res.push_back(curr);
+
+            // Find neighbours of popped element of queue
+            for(auto &nbr : adj_list[curr])
+            {
+                // Update indegree map
+                indegree[nbr] -= 1;
+                if(indegree[nbr] == 0)
+                {
+                    topo_q.push(nbr);
+                }
+            }
+        }
+
+        // Update indegree map and repeat until no more elements remain in indegree
+        for(vector<int>::iterator it = res.begin(); it != res.end(); it++)
+            cout << *it << " ";
+
+        cout << "\n";
+
+        return res;
+    }
+    void check_cycle(int v, int e)
+    {
+        vector<int> indegree(v);
+        int cnt = 0;
+        queue<int> node_q;
+        int curr;
+
+        // Update indegree vector
+        for(auto &node : adj_list)
+            for(auto &nbr : node.second)
+                indegree[nbr] += 1;
+
+        for(int i = 0; i < indegree.size(); i++)
+            if(indegree[i] == 0)
+                node_q.push(i);
+
+        while(!node_q.empty())
+        {
+            curr = node_q.front();
+            node_q.pop();
+            cnt += 1;
+            for(auto &nbr : adj_list[curr])
+            {
+                indegree[nbr] -= 1;
+                if(indegree[nbr] == 0)
+                    node_q.push(nbr);
+            }
+        }
+        if(cnt == v)
+            cout << "Cycle is not present!" << "\n";
+        else
+            cout << "Cycle is present!!!" << "\n";
+    }
     void print_topo()
     {
         while(!topo_stk.empty())
@@ -57,6 +138,9 @@ private:
 int main()
 {
     graph g1;
+
+    // Case 1:
+    g1.add_edge(0, 1, 1);
     g1.add_edge(1, 2, 1);
     g1.add_edge(2, 3, 1);
     g1.add_edge(2, 4, 1);
@@ -64,13 +148,26 @@ int main()
     g1.add_edge(3, 8, 1);
     g1.add_edge(4, 5, 1);
     g1.add_edge(5, 6, 1);
-    g1.add_edge(4, 6, 1);
+    g1.add_edge(6, 4, 1);
     g1.add_edge(8, 7, 1);
+
+    g1.kahn_topo_sort(9, 10);
+    g1.check_cycle(9, 10);
+
+
+    // Case 2:
+    // g1.add_edge(0, 3, 1);
+    // g1.add_edge(0, 1, 1);
+    // g1.add_edge(1, 2, 1);
+    // g1.add_edge(3, 2, 1);
+    // g1.add_edge(2, 4, 1);
+
 
     // g1.print_graph();
 
-    g1.topological_sort(1);
-    g1.print_topo();
+    // g1.topological_sort(0);
+    // g1.kahn_topo_sort(5, 5);
+    // g1.check_cycle(5, 5);
 
 
     return 0;
